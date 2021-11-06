cf9b95353bf38c6f14db1df2ee803b11b82cf9e1
Joseph Martin <josephmartin1350@gmail.com>
2021-11-06 01:21:06 -0600

    redone

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
-// // Write your code here!
-
-// main.remove();
-
-// const newHeader = document.createElement('h1');
-
-// newHeader.setAttribute('id', 'victory');
-
-// newHeader.textContent = 'Joe is the champion';
-
-// document.body.append('h1');
-
-
-
-// const element = document.createElement('div');
-
-// document.body.append(element)
-
-// const ul = document.createElement('ul');
-
-// for (let i= 0; i< 3; i++) {
-//     const li = document.createElement('li');
-//     li.textContent = (i + 1).toString();
-//     ul.append(li)
-// }
-
-// element.append(ul);
-
-// const main = document.getElementById('main')
-// main.innerHTML = `
-//     <h3>
-//         An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective
-//     </h3>
-//     <p>
-//         <em>By: Byron Q. Poodle, Esq., BA.</em>
-//     </p>
-// `
-
-// body.append(main)
-
-
-
-main.remove('#main')
+const main = document.querySelector('main#main')
+main.remove()
 
 const newHeader = document.createElement('h1')
-newHeader.setAttribute('id', 'victory')
-newHeader.textContent = "Joe is the champion"
+newHeader.id = 'victory'
+newHeader.textContent = 'Joe is the champion'
+body.append(newHeader)
+
+
+
+
+
+// // // Write your code here!
+
+// // main.remove();
+
+// // const newHeader = document.createElement('h1');
+
+// // newHeader.setAttribute('id', 'victory');
+
+// // newHeader.textContent = 'Joe is the champion';
+
+// // document.body.append('h1');
+
+
+
+// // const element = document.createElement('div');
+
+// // document.body.append(element)
+
+// // const ul = document.createElement('ul');
+
+// // for (let i= 0; i< 3; i++) {
+// //     const li = document.createElement('li');
+// //     li.textContent = (i + 1).toString();
+// //     ul.append(li)
+// // }
+
+// // element.append(ul);
+
+// // const main = document.getElementById('main')
+// // main.innerHTML = `
+// //     <h3>
+// //         An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective
+// //     </h3>
+// //     <p>
+// //         <em>By: Byron Q. Poodle, Esq., BA.</em>
+// //     </p>
+// // `
+
+// // body.append(main)
+
+
+
+// main.remove('#main')
+
+// const newHeader = document.createElement('h1')
+// newHeader.setAttribute('id', 'victory')
+// newHeader.textContent = "Joe is the champion"
